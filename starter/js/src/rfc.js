56f2f2a732e0939989329b445caeb92db10fc975
RequiemDK <kok.chee.kean@outlook.com>
2018-04-17 09:52:00 +0800

    done w/o deode

---
+++ starter/js/src/rfc.js
@@ -0,0 +1,22 @@
+function encode(word, n) {
+    var result = [];
+    for (var j = 0; j < n; j++) {
+        result[j] = [];
+    };
+    var i = 0;
+    var direction = 1;
+    var tmp_arr = word.split('');
+    tmp_arr.forEach(function(letter){
+        result[i].push(letter)
+        if (i === 0) {
+            direction = 1;
+        } else if (i === n-1) {
+            direction = -1;
+        };
+        i += direction;
+    });
+    for (j = 0; j < n; j++) {
+        result[j] = result[j].join('');
+    };
+    return result.join('')
+}

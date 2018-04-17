6d533fa6c815b04d36aea25623482e953b50dae9
RequiemDK <kok.chee.kean@outlook.com>
2018-04-17 11:22:49 +0800

    decode done

--- starter/js/src/rfc.js
+++ starter/js/src/rfc.js
@@ -24,20 +24,33 @@ function encode(word, n) {
 
 function decode(word, n) {
     var result = [];
+    var direction = 1;
     var l = word.length;
     var cycleLength = 2*n - 2;
     var firstRowLength = Math.ceil(l/cycleLength);
     var middleRowLength = Math.floor(l/cycleLength) * 2;
     var bottomRowLength = Math.floor(l/cycleLength);
-    var initialInterval = 2*n - 3;
     var rows = [];
-    rows[0] = word.substr(0, firstRowLength);
+    rows[0] = word.substr(0, firstRowLength).split('');
     var j = 1;
     for (var i = firstRowLength; i < l; i += middleRowLength) {
         rows[j] = word.substr(i, middleRowLength);
+        rows[j] = rows[j].split('');
         j++;
     };
-    // Insert zig-zag logic here
+    
+    j = 0;
+    for (i = 0; i < word.length; i++) {
+        if (rows[j].length > 0) {
+            result.push(rows[j].shift());
+        };
+        if (j === n-1) {
+            direction = -1;
+        } else if (j === 0) {
+            direction = 1;
+        };
+        j += direction
+    }
+    return result.join('');
 };
 
-decode('WECRLTEERDSOEEFEAOCAIVDEN', 3)
